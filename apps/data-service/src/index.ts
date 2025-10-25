8e94658c6d74b7d54a2dc97e3ffac177eefadef4
Saurabh Prakash <saurabhp75@gmail.com>
2025-10-25 19:40:14 +0530

    Set up Hono app in data service.

--- apps/data-service/src/index.ts
+++ apps/data-service/src/index.ts
@@ -1,7 +1,8 @@
 import { WorkerEntrypoint } from 'cloudflare:workers';
+import { App } from './hono/app';
 
 export default class DataService extends WorkerEntrypoint<Env> {
 	fetch(request: Request) {
-		return new Response('Hello World!');
+		return App.fetch(request, this.env, this.ctx);
 	}
 }
