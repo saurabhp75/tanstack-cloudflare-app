812242336adcfec5f9e246ab10904d68bc674807
Saurabh Prakash <saurabhp75@gmail.com>
2025-10-25 19:58:00 +0530

    Access CF request data.

--- apps/data-service/src/hono/app.ts
+++ apps/data-service/src/hono/app.ts
@@ -3,7 +3,14 @@ import { Hono } from 'hono';
 export const App = new Hono<{ Bindings: Env }>();
 
 App.get('/:id', async (c) => {
+	console.log(JSON.stringify(c.req.raw.cf));
+	const cf = c.req.raw.cf;
+	const country = cf?.country;
+	const lat = cf?.latitude;
+	const long = cf?.longitude;
 	return c.json({
-		message: 'Hello World!',
+		country,
+		lat,
+		long,
 	});
 });
