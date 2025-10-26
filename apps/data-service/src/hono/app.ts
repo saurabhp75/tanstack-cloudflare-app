1fb701b463ca0d076029e7c93befc26b73de19cc
Saurabh Prakash <saurabhp75@gmail.com>
2025-10-26 12:53:18 +0530

    Bind user app D1 db to data service.

--- apps/data-service/src/hono/app.ts
+++ apps/data-service/src/hono/app.ts
@@ -1,16 +1,10 @@
+import { getLink } from '@repo/data-ops/queries/links';
 import { Hono } from 'hono';
 
 export const App = new Hono<{ Bindings: Env }>();
 
 App.get('/:id', async (c) => {
-	console.log(JSON.stringify(c.req.raw.cf));
-	const cf = c.req.raw.cf;
-	const country = cf?.country;
-	const lat = cf?.latitude;
-	const long = cf?.longitude;
-	return c.json({
-		country,
-		lat,
-		long,
-	});
+	const id = c.req.param('id');
+	const linkInfoFromDb = await getLink(id);
+	return c.json(linkInfoFromDb);
 });
